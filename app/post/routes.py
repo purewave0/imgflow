b65e96684b96b24882c5eb73870292f7acdcf9c8
purewave0 <purewave_@outlook.com>
2024-10-02 15:34:43 -0300

    feat: show relative time and full time of post

--- app/post/routes.py
+++ app/post/routes.py
@@ -7,9 +7,10 @@ from app.dbapi import get_post_and_media
 @bp.route('/<post_id>')
 def show_post(post_id):
     full_post = get_post_and_media(post_id)
+    print('\n\noi')
     return render_template(
         'post/index.html',
         post_title=full_post['title'],
-        post_created_on=full_post['created_on'],
+        post_created_on=full_post['created_on'].isoformat() + 'Z',
         media=full_post['media'],
     )
--- app/static/js/post.js
+++ app/static/js/post.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const DateTime = luxon.DateTime;
     const postCreationDate = document.getElementById('post-created-on');
-    postCreationDate.textContent = new Date(postCreationDate.textContent).toLocaleString();
+    const postCreatedOn = DateTime
+        .fromISO(postCreationDate.textContent.trim());
+
+    postCreationDate.textContent = postCreatedOn.toRelative();
+    postCreationDate.title = postCreatedOn
+        .toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
 });
