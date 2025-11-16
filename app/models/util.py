d3fdab68a5e7a0c5a64ff0a50f5cb6beccbae926
purewave0 <purewave_@outlook.com>
2025-11-15 22:47:09 -0300

    feat(db): add utcnow util function for SQLite too

--- app/models/util.py
+++ app/models/util.py
@@ -5,9 +5,16 @@ from app.extensions import db
 
 
 class utcnow(expression.FunctionElement):
+    """The current time in UTC."""
     type = db.DateTime()
     inherit_cache = True
 
 @compiles(utcnow, 'mariadb')
-def pg_utcnow(element, compiler, **kw):
+def mariadb_utcnow(element, compiler, **kw):
+    """Return the current time in UTC for MariaDB."""
     return "UTC_TIMESTAMP"
+
+@compiles(utcnow, 'sqlite')
+def sqlite_utcnow(element, compiler, **kw):
+    """Return the current time in UTC for SQLite."""
+    return "CURRENT_TIMESTAMP"
