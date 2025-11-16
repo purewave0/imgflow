d248df5fb9f5772c8437f70a3ce414c3c5134227
purewave0 <purewave_@outlook.com>
2025-11-15 22:45:53 -0300

    docs(db/user): document models

--- app/models/user.py
+++ app/models/user.py
@@ -9,6 +9,7 @@ from app.models.util import utcnow
 
 
 class User(UserMixin, db.Model):
+    """A registered user."""
     __tablename__ = 'User'
     MIN_NAME_LENGTH = 2
     MAX_NAME_LENGTH = 32
@@ -18,9 +19,13 @@ class User(UserMixin, db.Model):
     name = db.Column(
         db.String(MAX_NAME_LENGTH), nullable=False, unique=True, index=True
     )
+    """The name of the user, used in URLs."""
     password_hash = db.Column(db.String(256))
+    """The hashed password."""
     created_on = db.Column(db.DateTime, server_default=utcnow())
+    """When the user was created."""
     score = db.Column(db.Integer, nullable=False, default=0)
+    """How many times this user's post and comments were upvoted."""
     # TODO: profile picture (avatar)
     # avatar_url = db.Column(db.String(128), nullable=True)
 
@@ -38,9 +43,10 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f'<User "{self.name}">'
 
-
     def set_password(self, password: str):
+        """Set the password."""
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password: str) -> bool:
+        """Return True if the given password matches the stored one."""
         return check_password_hash(self.password_hash, password)
